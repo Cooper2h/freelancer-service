09822ad5dd73dda2345f7849cad031106dae5478
Cooper <step.stepstep@yandex.ru>
2024-10-07 15:56:24 +0300

    Добавлен csv

--- src/test/java/FreelancerServiceTest.java
+++ src/test/java/FreelancerServiceTest.java
@@ -1,15 +1,17 @@
+package ru.netology.services;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import ru.netology.services.FreelancerService;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class FreelancerServiceTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "10000, 3000, 20000, 3", // Первый набор данных: доход, траты, порог, ожидаемый результат
-            "100000, 60000, 150000, 2" // Второй набор данных: доход, траты, порог, ожидаемый результат
-    })
+    // @CsvSource({
+    //        "10000, 3000, 20000, 3", // Первый набор данных: доход, траты, порог, ожидаемый результат
+    //       "100000, 60000, 150000, 2" // Второй набор данных: доход, траты, порог, ожидаемый результат
+    //    })
+    @CsvFileSource(files="src/test/resources/test-data.csv")
     public void testCalculate(int income, int expenses, int threshold, int expected) {
         FreelancerService service = new FreelancerService();
         // Фактический результат
